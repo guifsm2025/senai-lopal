14b5a0fe5b139843b5becf4bd57900ecf0fea114
guifsm2025 <guifsm2025@gmail.com>
2026-09-09 17:13:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+// let nota = Number(prompt("Digite a sua nota: "))
+
+// if (nota >= 6) {
+//     //bloco de código ->
+//     alert("aprovado")
+// } else if (nota >=4) {
+//     alert("recuperação!!!")
+// } else {
+//     alert("repovado")
+// }
+
+// let idade = Number(prompt("digite sua idade"))
+
+// if (idade <= 12) {
+//     alert("criança")
+// } else if (idade <= 18) {
+//     alert("adolescente")
+// } else if (idade <= 60) {
+//     alert("adulto")
+// } else { 
+//     alert("idoso")
+// }
+    
+// let valor = Number(prompt("digite seu valor"))
+
+// if (valor <= 100) {
+//     alert ("sem desconto")
+// } else if (valor <= 299.99) {
+//     let desconto = valor * 0.1
+//     let valor_total = valor - desconto
+//     alert (`10% de desconto: ${valor_total}`)
+// } else if (valor <= 499.99) {
+//     valor_total = valor * 0.8
+//     alert (`20% de desconto ${valor_total}`)
+//   } else {
+//     valor_total = valor * 0.7
+//     alert (`30% de desconto ${valor_total}`)
+//     }
+  
+    let valor = Number (prompt("qual o desocnto desse valor"))
+    
+    if (valor <=100) { 
+        alert ("não tem desconto")
+    } else if (valor <=299.99) {
+        let desconto = valor 
+        alert 
+    }
